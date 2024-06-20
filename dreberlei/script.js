82e7f280e965e7041a66f4792207c5afba36093d
kakau1401 <160244211+kakau1401@users.noreply.github.com>
2024-06-20 08:11:56 -0300

    Carga inicial

---
+++ dreberlei/script.js
@@ -0,0 +1,41 @@
+// var a = 78;
+// var b = 15;
+
+// var total = a + b;
+// console.log(total)
+
+// var a = 78;
+// var b = 15;
+
+// var total = a - b;
+// console.log(total)
+
+// var a = 78;
+// var b = 15;
+
+// var total = a * b;
+// console.log(total)
+
+// var a = 78;
+// var b = 15;
+
+// var total = a / b;
+// console.log(total)
+
+// var a = 78;
+// var b = 15;
+
+// var total = a % b;
+// console.log(total)
+
+// var a = 78;
+// var b = 15;
+
+// var total = a ++ b;
+// console.log(total)
+
+var a = 78;
+var b = 15;
+
+var total = a -- b;
+console.log(total)
